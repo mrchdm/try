5b1c9e96a12eb973e840c16f98baf8fc686a9aee
mrchdm <dementevamm@yandex.ru>
2022-09-29 23:39:00 +0300

    Создали исходный массив

---
+++ task/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Сколько элементов в массиве вы хотите видеть");
+int a = Convert.ToInt32(Console.ReadLine());
+
+string[] array = new string[a];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine($"Введите {i} элемент массива");
+    array[i] = Convert.ToString(Console.ReadLine());
+
+}
